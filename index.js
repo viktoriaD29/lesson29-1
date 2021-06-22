5b23a0bd3b9e43cb995c014f4ef281611efe2109
Viktoria <doduchenko29@gmail.com>
2021-06-22 12:22:31 +0300

    add

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ export const addImage = (imgSrc, callback) => {
   container.append(imgElem);
   imgElem.setAttribute('alt', 'My photo');
   imgElem.src = imgSrc;
+
   const onImageLoaded = () => {
     const { width, height } = imgElem;
     callback(null, { width, height });
   };
+
   imgElem.addEventListener('load', onImageLoaded);
   imgElem.addEventListener('error', () => callback('Image load is failed...'));
 };
+
 const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
@@ -20,6 +23,3 @@ const onImageLoaded = (error, data) => {
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;
 };
-/*const imgSrc =
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-addImage(imgSrc, onImageLoaded);*/
