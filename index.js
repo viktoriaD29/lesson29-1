200a3772dbb5feaecd448f73b6f9735778b19ae8
Viktoria <doduchenko29@gmail.com>
2021-06-22 12:11:48 +0300

    add

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ const onImageLoaded = (error, data) => {
 };
 const imgSrc =
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-addImage(imgSrc, onImageLoaded);
+//addImage(imgSrc, onImageLoaded);
